5ed3a18eac4d7c1aa78e60e798d89efa78bf6e7a
Carol Cruz <contato@carolcruzfotografia.com>
2026-09-21 16:00:57 -0300

    Adicionada configuracoes de seguranca

---
+++ src/main/java/com/javanauta/agendadotarefas/business/dto/UsuarioDTO.java
@@ -0,0 +1,20 @@
+package com.javanauta.agendadotarefas.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+
+
+
+}
